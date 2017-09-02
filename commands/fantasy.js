d2d3c25c914842b4417ddaf859e35676eac41f72
Doug Wade <douglas.b.wade@gmail.com>
2017-09-02 16:37:03 -0700

    Add fantasy

--- commands/fantasy.js
+++ commands/fantasy.js
@@ -4,39 +4,96 @@ const path = require('path');
 const untildify = require('untildify');
 
 module.exports = async (argv) => {
-  const {directory, player, date, dbPath, verbose} = argv;
+  const {directory, player, date, dbPath, verbose, all} = argv;
   if (verbose) {
     console.info(`using database at ${dbPath} to calculate points for ${player} on ${date}`);
   }
 
   await db.open(dbPath, { Promise });
 
-  const data = await db.get(`
-    select *
-    from outfieldPlayerMatchLog
-    inner join players
-    on players.playerid = outfieldPlayerMatchLog.playerid
-    inner join matches
-    on matches.matchid = outfieldPlayerMatchLog.matchid
-    where players.name = '${player}'
-    and matches.date = ${(new Date(date)).getTime()}
-  `);
+  let data;
+  if (player === 'all') {
+    console.log('all');
+    data = await db.all(`
+      select *
+      from outfieldPlayerMatchLog
+      inner join players
+      on players.playerid = outfieldPlayerMatchLog.playerid
+      inner join matches
+      on matches.matchid = outfieldPlayerMatchLog.matchid
+      where matches.date = ${(new Date(date)).getTime()}
+    `);
 
+    let i = 0;
+    const result = data.map(datum => {
+      if (verbose) {
+        console.log(`${JSON.stringify(datum)}`);
+      }
+      return { points: calculatePoints(datum), player: datum.name };
+    }).reduce((acc, elem) => {
+      if (verbose) {
+        console.log(`elem: ${JSON.stringify(elem)}`);
+      }
+      if (!acc[elem.points]) {
+        acc[elem.points] = []
+      }
+      acc[elem.points].push(elem);
+      return acc;
+    }, {});
+
+    Object.keys(result).reduce((acc, elem) => {
+      if (verbose) {
+        console.log(`elem: ${JSON.stringify(elem)}`);
+      }
+      if (acc < 0) {
+        acc.unshift(elem);
+      } else {
+        acc.push(elem);
+      }
+      return acc;
+    }, []).forEach((index) => {
+      const playerArr = result[index];
+      if (verbose) {
+        console.log(`playerArr: ${JSON.stringify(playerArr)}`);
+      }
+      playerArr.forEach(({ points, player}, i) => {
+        console.log(`${index + 1}.) ${player} scored ${points} fantasy points`);
+      });
+      i += playerArr.length;
+    });
+
+  } else {
+    data = await db.get(`
+      select *
+      from outfieldPlayerMatchLog
+      inner join players
+      on players.playerid = outfieldPlayerMatchLog.playerid
+      inner join matches
+      on matches.matchid = outfieldPlayerMatchLog.matchid
+      where players.name = '${player}'
+      and matches.date = ${(new Date(date)).getTime()}
+    `);
+    const points = calculatePoints(data);
+    console.log(`${data.player} scored ${points} fantasy points`);
+  }
+}
+
+function calculatePoints({ appearance, assists, foulsssuffered, goals, minutes, data, position, reds, shots, yellows }) {
   // From https://fantasy.mlssoccer.com/a/help
   let points = 0;
-  if (data.minutes >= 60) {
+  if (minutes >= 60) {
     points += 2;
-  } else if (['Started', 'Subbed on '].includes(data.appearance)) {
+  } else if (['Started', 'Subbed on '].includes(appearance)) {
     points += 1;
   }
 
-  if (['Defender', 'Goalkeeper', 'Defender/Midfielder', 'Midfielder/Defender'].includes(data.postition)) {
-    points += (data.goals * 6);
+  if (['Defender', 'Goalkeeper', 'Defender/Midfielder', 'Midfielder/Defender'].includes(position)) {
+    points += (goals * 6);
   } else {
-    points += (data.goals * 5);
+    points += (goals * 5);
   }
 
-  points += (data.assists * 3);
+  points += (assists * 3);
 
   // TODO: Add clean sheets and goals conceded (#18)
 
@@ -44,13 +101,13 @@ module.exports = async (argv) => {
 
   // TODO: Add own goals (#20)
 
-  points -= (data.yellows);
-  points -= (3 * data.reds);
+  points -= (yellows);
+  points -= (3 * reds);
 
   // TODO: Passing accuracy
 
-  points += Math.floor(data.shots/4);
-  points += Math.floor(data.foulsssuffered/4);
+  points += Math.floor(shots / 4);
+  points += Math.floor(foulsssuffered / 4);
 
-  console.log(`${player} scored ${points} fantasy points`);
+  return points;
 }
